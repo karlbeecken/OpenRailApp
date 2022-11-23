5154f276ae5d7d6fdf31384857c8a598efab3320
Karl Beecken <karl@beecken.berlin>
2022-11-23 21:47:06 +0100

    Initial Commit

---
+++ OpenRail/OpenRailApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenRailApp.swift
+//  OpenRail
+//
+//  Created by Karl Beecken on 23.11.22.
+//
+
+import SwiftUI
+
+@main
+struct OpenRailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
